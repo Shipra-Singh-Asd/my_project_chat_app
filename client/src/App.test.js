96124d29bd37ab6bc9afe12717b6b025de0b7819
“I528923” <shipra.singh01@sap.com>
2025-03-02 16:44:45 +0530

    Update Test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("allows user to enter name and join chat", async () => {
+  render(<App />);  
+  global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
+
+
+  const nameInput = screen.getByPlaceholderText(/Name/i);
+  const roomInput = screen.getByPlaceholderText(/Room ID/i);
+  const joinButton = screen.getByRole("button", { name: /Join the Room/i });
+
+  fireEvent.change(nameInput, { target: { value: "John" } });  
+  fireEvent.change(roomInput, { target: { value: "123" } });
+  fireEvent.click(joinButton);
+
+  expect(screen.queryByText(/Join A chat/i)).not.toBeInTheDocument();
 });
